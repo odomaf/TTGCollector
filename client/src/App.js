983abb7dde2c0d787d1a25070342b9f088ae2c97
Anne Odom <afodom207@gmail.com>
2026-03-16 20:17:45 -0500

    UPDATE: add import for AddGame component to app.js, remove line from index.js that came with scaffolding but that we don't need

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 
+//components
+import { AddGame } from './components/AddGame';
+
 function App() {
   return (
     <div className="App">
       <h1>Welcome to TTG Collector</h1>
+      <AddGame />
     </div>
   );
 }
