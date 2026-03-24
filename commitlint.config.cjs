5ba60783776a610e61d405c71e5bf80a5b5eeff3
Anne Odom <afodom207@gmail.com>
2026-03-24 16:37:58 -0500

    chore(tooling): configure husky commit-msg hok and commitlint

---
+++ commitlint.config.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      ["feat", "fix", "refactor", "style", "docs", "test", "chore"],
+    ],
+    "scope-enum": [
+      2,
+      "always",
+      [
+        "api",
+        "bgg",
+        "add-game",
+        "models",
+        "db",
+        "ui",
+        "modal",
+        "search",
+      ],
+    ],
+    "subject-full-stop": [2, "never", "."],
+    "header-max-length": [2, "always", 72],
+  },
+};
