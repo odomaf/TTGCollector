445a29d0259102590734364e63cb33ba3dd97f85
Anne Odom <afodom207@gmail.com>
2026-03-23 19:59:26 -0500

    MODIFY: Update to add bgg route info

--- server/controllers/api/index.js
+++ server/controllers/api/index.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
-const gameRoutes = require('./game-routes');
+const gameRoutes = require("./game-routes");
+const bggRoutes = require("./bgg-routes");
 
-router.use('/', gameRoutes);
+router.use("/", gameRoutes);
+router.use("/bgg", bggRoutes); // New BGG routes
+
 module.exports = router;
